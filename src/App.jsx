319662e2c8569e5d9d1daa7ac19776661c310bf5
Celiz Fernando <celizfernando82@gmail.com>
2026-07-02 00:09:10 -0500

    comentario1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-// App.jsx
+import './App.css';
 import CatalogoFetch from './components/CatalogoFetch';
 import CatalogoAxios from './components/CatalogoAxios';
 import CatalogoAlova from './components/CatalogoAlova';
  
 function App() {
   return (
-    <div style={{ display: 'flex', gap: '2rem' }}>
-      <CatalogoFetch />
-      <CatalogoAxios />
-      <CatalogoAlova />
+    <div className="container">
+      <div className="card">
+        <CatalogoFetch />
+      </div>
+      
+      <div className="card">
+        <CatalogoAxios />
+      </div>
+      
+      <div className="card">
+        <CatalogoAlova />
+      </div>
     </div>
   );
 }
- 
 export default App;
